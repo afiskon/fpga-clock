a1dd906ee805e34aa0204d27dc82aa90c7e665f7
Aleksander Alekseev <afiskon@gmail.com>
2017-12-03 00:40:22 +0300

    Replace tabs with spaces

--- main.sv
+++ main.sv
@@ -1,9 +1,10 @@
+/* vim: set ai et ts=4 sw=4: */
 `default_nettype none
 
 module encode_digit(
-		input logic [3:0] digit,
-		input logic hide,
-		output logic [0:6] seg);
+        input logic [3:0] digit,
+        input logic hide,
+        output logic [0:6] seg);
     assign seg = (hide == 1) ? 0 :
                     (digit == 0) ? 8'b1111110:
                     (digit == 1) ? 8'b0110000:
@@ -19,10 +20,10 @@ module encode_digit(
 endmodule
 
 module inc_minute(
-		input logic [3:0] min1,
-		input logic [3:0] min2,
+        input logic [3:0] min1,
+        input logic [3:0] min2,
         output logic [3:0] out_min1,
-		output logic [3:0] out_min2);
+        output logic [3:0] out_min2);
     assign out_min1 = (min1 == 9) ? 0 : min1 + 1;
     assign out_min2 = (min1 == 9) ?
         ( (min2 == 5) ? 0 : min2 + 1 )
@@ -30,10 +31,10 @@ module inc_minute(
 endmodule
 
 module inc_hour(
-		input logic [3:0] hour1,
-		input logic [3:0] hour2,
+        input logic [3:0] hour1,
+        input logic [3:0] hour2,
         output logic [3:0] out_hour1,
-		output logic [3:0] out_hour2);
+        output logic [3:0] out_hour2);
     assign out_hour1 =
         (((hour2 != 2)&&(hour1 == 9)) || ((hour2 == 2)&&(hour1 == 3)))
         ? 0 : hour1 + 1;
@@ -44,11 +45,11 @@ module inc_hour(
 endmodule
 
 module top(
-		input logic clk,
+        input logic clk,
         input logic btn_set,
-		input logic btn_inc,
+        input logic btn_inc,
         output logic [0:7] seg,
-		output logic [0:3] d);
+        output logic [0:3] d);
     logic [23:0] divider = 0;
     logic [3:0] d_rot = 4'b1110;
     logic [5:0] sec = 0;
@@ -141,11 +142,11 @@ module top(
     assign d = d_rot;
 
     logic [3:0] disp =
-		(d_rot == 4'b1110) ? min1:
-		(d_rot == 4'b1101) ? min2:
-		(d_rot == 4'b1011) ? hour1:
-		(d_rot == 4'b0111) ? hour2:
-		13; // should never happen
+        (d_rot == 4'b1110) ? min1:
+        (d_rot == 4'b1101) ? min2:
+        (d_rot == 4'b1011) ? hour1:
+        (d_rot == 4'b0111) ? hour2:
+        13; // should never happen
 
     logic hide_seg = (((current_set == 1) &&
                       ((d_rot == 4'b0111) || (d_rot == 4'b1011))) ||
